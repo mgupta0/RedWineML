0a7396a0126c967f1c00d968577fada2d0a32d27
mgupta0 <mayank@gonuts.com>
2024-03-02 15:10:20 +0000

    folder structure

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for ml app",
-    long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
